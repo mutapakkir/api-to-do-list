fc431afb935df36844d4500d30fdff8930b1daf1
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2024-12-30 07:32:56 -0800

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,1 +1,27 @@
+//const mySql = require('mysql2')
+//require('dotenv').config()
 
+//const pool = mySql.createPool({
+  //  host : process.env.DB_HOST,
+    //user : process.env.DB_USER,
+    //password :process.env.DB_PASS,
+    //database :process.env.DB_NAME,
+    //port : process.env.DB_PORT
+
+//})
+//const poolPromise = pool.promise()
+
+//module.exports = poolPromise
+require("dotenv").config()
+const mysql = require("mysql2")
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+const poolPromise = pool.promise()
+
+module.exports = poolPromise
