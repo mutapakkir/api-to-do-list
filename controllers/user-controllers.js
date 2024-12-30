b871389afdc70d9c55db938aeef9ef585120a4c2
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2024-12-30 07:34:32 -0800

    Add files via upload

---
+++ controllers/user-controllers.js
@@ -0,0 +1,121 @@
+//const userModel = require('../models/user_models')
+
+//const getAllStudent = async (req,res) => {
+  //  try{
+    //    const students = await userModel.getAllStudent();
+      //  if(!student)res.json({ message : 'Student not Found'})
+        //    res.json(student)
+    //}
+    //catch(err){
+     //   res.json({ message : 'Eror Get All Student'})  
+    //}
+    
+//};
+//const getStudentById = async(req,res) => {
+  //  const {id} = req.params;
+    //try{
+      //  const student = await userModel.getStudentById(req.params.id);
+        //if (student){
+          //  res.json({ succes : true, data : student});        
+        //} else {
+          //  res.status(404).json({ succes : false, message :'Student not Found'})
+        //}
+    //} catch (err){
+      //  res.status(500).json({succes : false, message : err.message})
+    //}
+//}
+
+//const getStudentById = async(req,res)=>{
+  //  try{
+    //    const student =
+      //  await userModel.getStudentById(req.params.id)
+        //if (student.length >0){
+          //  res.status(200).json({message : 'succes',student})
+        //}
+        //else{
+          //  res.status(500).json({message : 'student not found'})
+            
+        //}
+    //}catch(eror){
+      //  console.log(eror);
+        //res.status(500).json({message : 'Eror Get Student'})
+        
+    //}
+//}
+//const addStudent = async (req,res) => {
+  //  try{
+    //    const newStudent =
+      //  await userModel.addStudent(req.body)
+        //res.status(200).json({id:newStudent,...req.body})
+    //}catch (eror){
+      //  console.log(eror);
+        //res.status(500).json({message:'eror insert student'})
+        
+    //}
+//}
+//module.exports = {
+  //  getAllStudent,
+    //getStudentById,
+    //addStudent}
+    const userModel = require("../models/user-models")
+
+const getAllStudent = async (req, res) => {
+    try {
+        const student =
+            await userModel.getAllStudent()
+        if (!student) res.json({ message: 'Student Not Found' })
+        res.json(student)
+    }
+    catch (error) {
+        console.log(error);
+        req.json({ message: 'Error Get All Student' })
+    }
+}
+
+const getStudentById = async (req, res) => {
+    try {
+        const student =
+            await userModel.getStudentById(req.params.id)
+        if (student.length > 0) {
+            res.status(200).json({ message: 'Sukses', student })
+        }
+        else {
+            res.status(500).json({ message: 'Student Not Found' })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error Get Student' })
+    }
+}
+
+const addStudent = async (req, res) => {
+    try {
+        const newStudent = await userModel.addStudent(req.body)
+        res.status(200).json({ id: newStudent, ...req.body })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error Insert Student' })
+    }
+}
+
+const deleteStudent = async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedCount = await userModel.deleteStudentById(id)
+        if (deletedCount > 0) {
+            res.status(200).json({ message: "Student deleted successfully" })
+        } else {
+            res.status(404).json({ message: "Student not found" })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports = {
+    getAllStudent,
+    getStudentById,
+    addStudent,
+    deleteStudent
+}
